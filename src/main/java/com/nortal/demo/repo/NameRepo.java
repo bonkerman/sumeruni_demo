449e8120e9240f7c332072ba6cf4619a27400daf
Markus Benediktas Ridziauskas <markus.ridziauskas@nortal.com>
2021-06-15 12:40:16 +0300

    exception throwing

--- src/main/java/com/nortal/demo/repo/NameRepo.java
+++ src/main/java/com/nortal/demo/repo/NameRepo.java
@@ -3,7 +3,9 @@ package com.nortal.demo.repo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import lombok.extern.log4j.Log4j2;
 
 @Service
@@ -15,7 +17,8 @@ public class NameRepo implements Repository {
 	@Override
 	public void save(final String name) {
 		if (names.contains(name)) {
-			return;
+			log.info("Name {} already exists", name);
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Name already exists");
 		}
 		names.add(name);
 		log.info("Name {} is saved", name);
@@ -24,7 +27,8 @@ public class NameRepo implements Repository {
 	@Override
 	public void delete(final String name) {
 		if (!names.contains(name)) {
-			return;
+			log.info("There is no such name to delete");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There is no such name to delete");
 		}
 		names.remove(name);
 		log.info("Name {} is deleted", name);
